61ea9a0d13b4e21b889407e9e28cb6e12f3cbbaf
Agu Orcha <aguorcha@gmail.com>
2023-03-21 13:43:24 +0100

    Algunas modificaciones intentando evitar que las flechas miren hacia arriba al cargar la página. A partir de que se hace click, sí que funciona correctamente la dirección de las flechas

--- main.js
+++ main.js
@@ -1,31 +1,34 @@
-const spans = document.querySelectorAll('.faq-title');
+document.addEventListener('DOMContentLoaded', function(event) {
+  const spans = document.querySelectorAll('.faq-title');
 
-spans.forEach(span => {
-  span.addEventListener('click', () => {
-    const p = span.nextElementSibling;
+  spans.forEach(span => {
+    span.addEventListener('click', () => {
+      const p = span.nextElementSibling;
 
-    if (p && p.style.display === 'none') {
-      p.style.display = 'block';
+      if (p && p.style.display === 'none') {
+        p.style.display = 'block';
 
-      const paragraphs = document.querySelectorAll('.faq-content');
-      paragraphs.forEach(paragraph => {
-        if (paragraph !== p) {
-          paragraph.style.display = 'none';
-        }
-      });
+        const paragraphs = document.querySelectorAll('.faq-content');
+        paragraphs.forEach(paragraph => {
+          if (paragraph !== p) {
+            paragraph.style.display = 'none';
+          }
+        });
 
-      spans.forEach(s => {
-        s.classList.remove('active');
-        s.querySelector('span').classList.remove('rotate');
-      });
+        spans.forEach(s => {
+          s.classList.remove('active');
+          s.querySelector('span').classList.remove('rotate');
+        });
 
-      span.classList.add('active');
-      span.querySelector('span').classList.add('rotate');
+        span.classList.add('active');
+        span.querySelector('span').classList.add('rotate');
 
-    } else if (p) {
-      p.style.display = 'none';
-      span.classList.remove('active');
-      span.querySelector('span').classList.remove('rotate');
-    }
+      } else if (p) {
+        p.style.display = 'none';
+        span.classList.remove('active');
+        span.querySelector('span').classList.remove('rotate');
+      }
+    });
   });
 });
+
